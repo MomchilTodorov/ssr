99638213a92b36fe5493281a3bb9f63eb48bfa72
MomchilTodorov <52608910+MomchilTodorov@users.noreply.github.com>
2020-10-17 22:27:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use(
   "/api",
   proxy("http://react-ssr-api.herokuapp.com", {
     proxyReqOptDecorator(opts) {
-      opts.headers["x-forwarded-host"] = "localhost:3000";
+      opts.headers["x-forwarded-host"] = "ssrappreact.herokuapp.com";
       return opts;
     },
   })
